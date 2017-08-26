135830eb6d3db2f87a5d1faa10c2e0428a6b4ed7
danyalattari <danyal823@gmail.com>
2017-08-26 15:31:43 +0500

    portfolio

---
+++ app.js
@@ -0,0 +1,181 @@
+var obj = document.getElementById('obj');
+var year1 = document.getElementById('year1');
+var uni1 = document.getElementById('uni1');
+var degree1 = document.getElementById('degree1');
+var year2 = document.getElementById('year2');
+var uni2 = document.getElementById('uni2');
+var degree2 = document.getElementById('degree2');
+var year3 = document.getElementById('year3');
+var uni3 = document.getElementById('uni3');
+var degree3 = document.getElementById('degree3');
+var sal1 = document.getElementById('sal1');
+var com1 = document.getElementById('com1');
+var post1 = document.getElementById('post1');
+var sal2 = document.getElementById('sal2');
+var com2 = document.getElementById('com2');
+var post2 = document.getElementById('post2');
+var sal3 = document.getElementById('sal3');
+var com3 = document.getElementById('com3');
+var post3 = document.getElementById('post3');
+var skil1 = document.getElementById('skil1');
+var skil2 = document.getElementById('skil2');
+var skil3 = document.getElementById('skil3');
+var skil4 = document.getElementById('skil4');
+var train = document.getElementById('train');
+var hob1 = document.getElementById('hob1');
+var hob2 = document.getElementById('hob2');
+var hob3 = document.getElementById('hob3');
+
+
+var name1 = document.getElementById('name1');
+var add = document.getElementById('add');
+var contact = document.getElementById('contact');
+var birth = document.getElementById('birth');
+var lang1 = document.getElementById('lang1');
+var lang2 = document.getElementById('lang2');
+var lang3 = document.getElementById('lang3');
+var cs1 = document.getElementById('cs1');
+var cs2 = document.getElementById('cs2');
+var cs3 = document.getElementById('cs3');
+var pers1 = document.getElementById('pers1');
+var pers2 = document.getElementById('pers2');
+var pers3 = document.getElementById('pers3');
+
+
+var cv = {
+    obj: 'To be a part of company that indulges professionel growth which provides challenging and regarding career while allowing me to utilize my knowledge and skills.',
+    year1: '2017-2018',
+    uni1: 'Karachi University',
+    degree1: 'M.A in Islamiat',
+    year2: '2010-2012',
+    uni2: 'S.M Arts & Commerce College',
+    degree2: 'Bachelor In Commerce',
+    year3: '2007-2009',
+    uni3: 'S.M Arts & Commerce College',
+    degree3: 'Inter in Commerce',
+    sal1: '06,2016-08,2017',
+    com1: 'S.Traders Company',
+    post1: 'Accountant',
+    sal2: '01,2016-05,2016',
+    com2: 'S.Traders Company',
+    post2: 'Sub Accountant',
+    sal3: '08,2015-12,2015',
+    com3: 'A & B Industries',
+    post3: 'Sub Manager',
+    skil1: 'Innovation Management',
+    skil2: 'Analytical Thinking',
+    skil3: 'Email Marketing',
+    skil4: 'Social Media Marketing',
+    train: 'ABC Company',
+    hob1: 'Using Internet',
+    hob2: 'Play video games',
+    hob3: 'Books Read',
+    name1: 'Danyal Raza',
+    add: 'Acchi Qabar',
+    contact: '0300-1234567 <br> danyal.attari@yahoo.com',
+    birth: '09-jan,2017',
+    lang1: 'English',
+    lang2: 'Urdu',
+    lang3: 'Arabic',
+    cs1: 'M-S Word',
+    cs2: 'M-S Excel',
+    cs3: 'M-S PowerPoint',
+    pers1: 'Caring and Sensitive',
+    pers2: 'Responsible',
+    pers3: 'Non Smoker'
+}
+
+obj.innerHTML = cv.obj;
+year1.innerHTML = cv.year1;
+uni1.innerHTML = cv.uni1;
+degree1.innerHTML = cv.degree1;
+year2.innerHTML = cv.year2;
+uni2.innerHTML = cv.uni2;
+degree2.innerHTML = cv.degree2;
+year3.innerHTML = cv.year3;
+uni3.innerHTML = cv.uni3;
+degree3.innerHTML = cv.degree3;
+sal1.innerHTML = cv.sal1;
+com1.innerHTML = cv.com1;
+post1.innerHTML = cv.post1;
+sal2.innerHTML = cv.sal2;
+com2.innerHTML = cv.com2;
+post2.innerHTML = cv.post2;
+sal3.innerHTML = cv.sal3;
+com3.innerHTML = cv.com3;
+post3.innerHTML = cv.post3;
+skil1.innerHTML = cv.skil1;
+skil2.innerHTML = cv.skil2;
+skil3.innerHTML = cv.skil3;
+skil4.innerHTML = cv.skil4;
+train.innerHTML = cv.train;
+hob1.innerHTML = cv.hob1;
+hob2.innerHTML = cv.hob2;
+hob3.innerHTML = cv.hob3;
+
+
+name1.innerHTML = cv.name1;
+add.innerHTML = cv.add;
+contact.innerHTML = cv.contact;
+birth.innerHTML = cv.birth;
+lang1.innerHTML = cv.lang1;
+lang2.innerHTML = cv.lang2;
+lang3.innerHTML = cv.lang3;
+cs1.innerHTML = cv.cs1;
+cs2.innerHTML = cv.cs2;
+cs3.innerHTML = cv.cs3;
+pers1.innerHTML = cv.pers1;
+pers2.innerHTML = cv.pers2;
+pers3.innerHTML = cv.pers3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var name1 = document.getElementById('name1');
+// var fName = document.getElementById('fName');
+// var email = document.getElementById('email');
+// var num = document.getElementById('num');
+// var add = document.getElementById('add');
+
+// var personal = {
+//     name1: 'danyal',
+//     fName: 'abdulAziz',
+//     email: 'a@a.com',
+//     num: 515552,
+//     add: 'acchiQabar'
+// }
+
+// name1.innerHTML = personal.name1;
+// fName.innerHTML = personal.fName;
+// email.innerHTML = personal.email;
+// num.innerHTML = personal.num;
+// add .innerHTML= personal.add;
+
+// var fsc = document.getElementById('fsc');
+// var hsc = document.getElementById('hsc');
+// var graduate = document.getElementById('graduate');
+// var master = document.getElementById('master');
+
+// var eduacation = {
+//     fsc: "It was Passed 80% <br> H.M School",
+//     hsc: "It was Passed 65% <br> S.M College",
+//     graduate: "It was Passed First Divison <br> S.M College",
+//     master: "It was Passed second Divison <br> Karachi University"
+// }
+// fsc.innerHTML = eduacation.fsc;
+// hsc.innerHTML = eduacation.hsc;
+// graduate.innerHTML = eduacation.graduate;
+// master.innerHTML = eduacation.master;
